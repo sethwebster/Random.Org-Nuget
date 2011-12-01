f19238cd78774ef63dfded885c424d61a5f4903d
sethwebster <sethwebster@gmail.com>
2011-12-01 09:53:17 -0700

    Adding local random mode for flights :)

---
+++ Random.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Random.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Random r = new Random();
+            var v = r.Next(100, 1000);
+
+            Assert.IsTrue(v >= 100 && v <= 1000);
+        }
+    }
+}
---
+++ Random/Random.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace Random
+{
+    // str = 
+
+    public class Random
+    {
+        WebClient cli = null;
+        string format = "http://www.random.org/integers/?num={0}&min={1}&max={2}&col=1&base=10&format=plain&rnd=new";
+        public bool UseLocalMode { get; set; }
+        private System.Random localRandom = new System.Random((int)DateTime.Now.Ticks);
+        public int Next(int min, int max)
+        {
+            if (!UseLocalMode)
+            {
+                cli = new WebClient();
+                var data = cli.DownloadString(
+                    string.Format(format, 1, min, max)
+                    );
+                if (string.IsNullOrEmpty(data))
+                {
+                    throw new Exception("No response from random.org");
+                }
+
+                int value = -1;
+                if (int.TryParse(data, out value))
+                {
+                    return value;
+                }
+                else
+                {
+                    throw new ArgumentException("The value returned from random.org was not properly formatted");
+                }
+            }
+            else
+            {
+                return localRandom.Next(min, max);
+            }
+        }
+    }
+}
