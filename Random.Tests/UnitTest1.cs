495740d284cef1710cf00d97f617513540633aeb
sethwebster <sethwebster@gmail.com>
2013-01-10 15:15:08 -0700

    Changed NameSpace

--- Random.Tests/UnitTest1.cs
+++ Random.Tests/UnitTest1.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Random.Tests
+namespace Random.Org.Tests
 {
     [TestClass]
     public class UnitTest1
--- Random/Random.cs
+++ Random/Random.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Net;
 
-namespace Random
+namespace Random.Org
 {
-    // str = 
-
     public class Random
     {
         WebClient cli = null;
         string format = "http://www.random.org/integers/?num={0}&min={1}&max={2}&col=1&base=10&format=plain&rnd=new";
+
+        /// <summary>
+        /// Gets or sets a value that, when false, prevents actually calling Random.org  and generates a local pseudo-random value -- useful for testing offline scenarios 
+        /// </summary>
         public bool UseLocalMode { get; set; }
         private System.Random localRandom = new System.Random((int)DateTime.Now.Ticks);
         public int Next(int min, int max)
